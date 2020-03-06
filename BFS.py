9e1b6e03fbfd4962fd7f80e324857f10fb7470d3
Or Daria <21261637+orDasha@users.noreply.github.com>
2020-03-06 23:44:09 +0300

    BFS

---
+++ BFS.py
@@ -0,0 +1,64 @@
+from _collections import deque
+
+graph_desc = [
+    [0, 1, 1, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 1, 0],
+]
+
+def bfs(graph, start, finish):
+    parent = [None for _ in range(len(graph))]
+    is_visited = [False for _ in range(len(graph))]
+
+    deq = deque([start])
+    is_visited[start] = True
+
+    while len(deq) > 0:
+
+        current = deq.pop()
+
+        if current == finish:
+            # return parent
+            break
+
+        for i, vertex in enumerate(graph[current]):
+            if vertex == 1 and not is_visited[i]:
+
+                is_visited[i] = True
+                parent[i] = current
+                deq.appendleft(i)
+
+    else:
+        return f'Из вершины {start} нельзя попасть в {finish}'
+
+    cost = 0
+    way = deque([finish])
+    i = finish
+
+    while parent[i] != start:
+        cost += 1
+        way.appendleft(parent[i])
+        i = parent[i]
+
+    cost += 1
+    way.appendleft(start)
+
+    return f'Кратчайший путь {list(way)} длиной в {cost} условных единиц'
+
+while True:
+    start_vertex = input('От какой вершины идти: ')
+    finish_vertex = input('До какой вершины идти: ')
+
+    if not start_vertex.isdigit():
+        continue
+
+    if not finish_vertex.isdigit():
+        continue
+
+    print(bfs(graph_desc, int(start_vertex), int(finish_vertex)))
+    break
